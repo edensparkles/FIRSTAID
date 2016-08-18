be240130b50a037a8e0368e9c93c7dbe7b3ee2c2
Eden Lawson <itzeden99@gmail.com>
2016-08-18 14:06:55 -0400

    pull eden

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 import pymongo
 from pymongo import MongoClient
+from flask import Flask
 import datetime
 client = MongoClient()
 
@@ -8,20 +9,34 @@ client = MongoClient('mongodb://localhost:27017/')
 db = client.test_database
 collection = db.test_collection
 
-posts = db.posts
-post = {"birthdate": birthday,
-        "name": full_name,
-        "number": phone_number,
+users = db.users
+user = {"birthdate": birthday,
+        "name": name,
+        "number": number,
         "email": email,
         "contact1":  contact1,
         "contact2": contact2,
         "contactname1": contactname1,
         "contactname2": contactname2,
-        
-        
-        
+        "contact1number": contact1number,
+        "contact2number": contact2number
+        }
         
 
-posts.insert_one(post)
-for i in posts.find():
+users.insert_one(user)
+for i in users.find():
     print(i)
+    
+    
+@app.route('/new_user')
+    def signup():
+        if not session.get(' birthday','name', 'number', 'email'):
+            abort(401)
+        new_user = {"birthdate": birthday,
+            "name": name,
+            "number": number,
+            "email": email
+            }
+        db.insert_one(new_user)
+        flash('Account successfully created')
+        return redirect('opening.html')
